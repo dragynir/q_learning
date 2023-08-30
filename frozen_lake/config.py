fd3ebcfe93502754edd044872106ac57c33a41fa
dragynir <d.korostelevdn@gmail.com>
2023-08-30 22:55:06 +0700

    init

---
+++ frozen_lake/config.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class Config:
+    env_name: str = 'FrozenLake-v1'
+    map_name: str = '4x4'
+    is_slippery: bool = False
+    render_mode: str = 'rgb_array'
+
+    # Training parameters
+    n_training_episodes: int = 10000  # Total training episodes
+    learning_rate: float = 0.7  # Learning rate
+
+    # Evaluation parameters
+    n_eval_episodes: int = 100  # Total number of test episodes
+
+    # Environment parameters
+    max_steps: int = 99  # Max steps per episode
+    gamma: float = 0.95  # Discounting rate
+
+    # Exploration parameters
+    max_epsilon: float = 1.0  # Exploration probability at start
+    min_epsilon: float = 0.05  # Minimum exploration probability
+    decay_rate: float = 0.0005  # Exponential decay rate for exploration prob
---
+++ frozen_lake/train.py
@@ -0,0 +1,85 @@
+import numpy as np
+import gymnasium as gym
+import random
+import imageio
+import os
+import tqdm
+
+from gym import Env
+from tqdm.notebook import tqdm
+
+from frozen_lake.config import Config
+import matplotlib.pyplot as plt
+
+from frozen_lake.q_learning import initialize_q_table, train_qtable, evaluate_agent, record_video, show_video
+
+
+def show_env_example(env: Env):
+    env.reset()
+    plt.figure()
+    plt.imshow(env.render())
+    plt.show()
+
+
+def train():
+    """
+    Main function for q-learning train
+
+
+    Example to create a custom map
+    desc = ['SFFF', 'FHFH', 'FFFH', 'HFFG']
+    gym.make('FrozenLake-v1', desc=desc, is_slippery=True)
+    """
+    config = Config()
+    env = gym.make(
+        config.env_name,
+        map_name=config.map_name,
+        is_slippery=config.is_slippery,
+        render_mode=config.render_mode,
+    )
+
+    print('_____OBSERVATION SPACE_____ \n')
+    print('Observation Space', env.observation_space)
+    print('Sample observation', env.observation_space.sample())  # Get a random observation
+    show_env_example(env)
+
+    state_space = env.observation_space.n
+    print('There are ', state_space, ' possible states')
+
+    action_space = env.action_space.n
+    print('There are ', action_space, ' possible actions')
+
+    Qtable_frozenlake = initialize_q_table(state_space, action_space)
+
+    print('_____TRAINING_____ \n')
+
+    Qtable_frozenlake = train_qtable(
+        env,
+        Qtable_frozenlake,
+        config.n_training_episodes,
+        config.min_epsilon,
+        config.max_epsilon,
+        config.decay_rate,
+        config.max_steps,
+        config.gamma,
+        config.learning_rate,
+    )
+
+    # Evaluate our Agent
+    eval_seed = [42] * config.n_eval_episodes
+    mean_reward, std_reward = evaluate_agent(
+        env,
+        config.max_steps,
+        config.n_eval_episodes,
+        Qtable_frozenlake,
+        eval_seed,
+    )
+    print(f"Mean_reward={mean_reward:.2f} +/- {std_reward:.2f}")
+
+    trained_video = './replays/trained.mp4'
+    record_video(env, Qtable_frozenlake, out_directory=trained_video, fps=1)
+    show_video(trained_video)
+
+
+if __name__ == '__main__':
+    train()
